fec770c96882f76cdaf65c74dc350c04a01095e2
Olena <113938017+Alenalenk@users.noreply.github.com>
2024-03-18 13:10:01 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
 (() => {
     "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(webP.height == 2);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = support === true ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
     function ssr_window_esm_isObject(obj) {
         return obj !== null && typeof obj === "object" && "constructor" in obj && obj.constructor === Object;
     }
@@ -3224,4 +3237,5 @@
     }
     checkingInformation();
     window["FLS"] = true;
+    isWebp();
 })();
